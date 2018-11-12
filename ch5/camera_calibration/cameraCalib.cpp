94817628f9c2e7b1b273d940cb35d39200e043d8
dingqingpeng <flyingstone11@163.com>
2018-11-12 16:46:52 +0800

    starting to code cameraCalib.cpp

--- ch5/camera_calibration/cameraCalib.cpp
+++ ch5/camera_calibration/cameraCalib.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <boost/format.hpp>
 
 using namespace std;
+using namespace cv;
+
+static void help()
+{
+    cout << "This is a camera calibration program." << endl;
+    cout << "Reference: OpenCV camera calibration sample." << endl;
+}
+
+Mat fileRead(int i)
+{
+    Mat result;
+    boost::format fmt( "../%s/%d.%s" );	// file format
+    result = imread( (fmt%"chessBoard"%(i+1)%"png").str(), IMREAD_COLOR );
+    return result;
+}
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    help();
+
+    // Get input
+    for(;;)
+    {
+        /* code */
+    }
+    
     return 0;
 }
