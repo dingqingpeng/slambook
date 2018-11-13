03ed0b57e3218e7edd60b273bc2b4a3fe86ce6d1
dingqingpeng <flyingstone11@163.com>
2018-11-13 18:11:34 +0800

    working on class Settings

--- ch5/camera_calibration/cameraCalib.cpp
+++ ch5/camera_calibration/cameraCalib.cpp
@@ -6,17 +6,47 @@
 using namespace std;
 using namespace cv;
 
+class Settings
+{
+public:
+    void read(const FileNode& node)
+    {
+        node["BoardSize_Width"]  >> boardSize.width;
+        node["BoardSize_Height"] >> boardSize.height;
+        node["Square_Size"]      >> squareSize;
+        node["Calibrate_Pattern"]>> patternToUse;
+        node["Calibrate_NrOfFrameToUse"] >> nrFrames;
+    }
+public:
+    Size  boardSize;     // Size of the board, number of items
+    float squareSize;    // The size of a square in some user defined metric system (pixel, millimeter)
+    string patternToUse;
+    int nrFrames;        // The number of frames to use for calibration
+};
+
+/* Brief: Print program info
+ * Parameters:
+ *     None
+ * Return:
+ *     Void
+ */
 static void help()
 {
     cout << "This is a camera calibration program." << endl;
     cout << "Reference: OpenCV camera calibration sample." << endl;
 }
 
+/* Brief: import image using cv::imread
+ * Parameters:
+ *     i -- iterator indicating file name
+ * Return:
+ *     result -- image in cv::Mat type
+ */
 Mat fileRead(int i)
 {
     Mat result;
     boost::format fmt( "../%s/%d.%s" );	// file format
-    result = imread( (fmt%"chessBoard"%(i+1)%"png").str(), IMREAD_COLOR );
+    result = imread( (fmt%"chessBoard"%(i+1)%"jpg").str(), IMREAD_COLOR );
     return result;
 }
 
@@ -24,11 +54,31 @@ int main(int argc, char const *argv[])
 {
     help();
 
-    // Get input
-    for(;;)
+    //! [Read configure file]
+    Settings s;
+    //! End of [Read configure file]
+
+    //! [Get input]
+    for(int i = 0; ; ++i)
     {
-        /* code */
+        Mat view;
+
+        view = fileRead(i);
+        if(view.empty())
+        {
+            break;
+        }
+        cout << i << ".jpg read" << endl;
     }
+    //! End of [Get input]
+
+    //! [Find pattern]
+    bool found;
+    found = findChessboardCorners( view,  )
+    //! End of [Find pattern]
     
     return 0;
 }
+
+
+// No more
