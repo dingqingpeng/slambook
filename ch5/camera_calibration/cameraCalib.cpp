58ffb32c9c2cda5c7a7f0c0f730557fb740ed0df
dingqingpeng <flyingstone11@163.com>
2018-11-14 20:09:29 +0800

    Version 0.1

--- ch5/camera_calibration/cameraCalib.cpp
+++ ch5/camera_calibration/cameraCalib.cpp
@@ -1,59 +1,10 @@
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc.hpp>
 #include <boost/format.hpp>
-
-using namespace std;
-using namespace cv;
-
-class Settings
-{
-public:
-    /* Brief: import data from filenode
-    * Parameters:
-    *     node -- reference to FileNode
-    * Return:
-    *     Void
-    */
-    void read(const FileNode& node)
-    {
-        node["BoardSize_Width"]  >> boardSize.width;
-        node["BoardSize_Height"] >> boardSize.height;
-        node["Square_Size"]      >> squareSize;
-        node["Calibrate_Pattern"]>> patternToUse;
-        node["Calibrate_NrOfFrameToUse"] >> nrFrames;
-        validate();
-    }
-    void validate()
-    {
-        validInput = true;
-        
-        if (boardSize.width <= 0 || boardSize.height<= 0) 
-        {
-            cerr << "Invalid board size." << endl;
-            validInput = false;
-        }
-        if (squareSize < 1e-6)
-        {
-            cerr << "Invalid square size." << endl;
-            validInput = false;
-        }
-        if (nrFrames <= 0)
-        {
-            cerr << "Invalid number of frame." << endl;
-            validInput = false;
-        }
-        
-        
-    }
-public:
-    Size  boardSize;     // Size of the board, number of items
-    float squareSize;    // The size of a square in some user defined metric system (pixel, millimeter)
-    string patternToUse;
-    int nrFrames;        // The number of frames to use for calibration
-    
-    bool validInput;     // True if the setting file passes validation
-};
+#include "cameraCalib.h"
 
 /* Brief: Print program info
  * Parameters:
@@ -81,33 +32,95 @@ Mat fileRead(int i)
     return result;
 }
 
+static inline void read(const FileNode& node, Settings& x, const Settings& default_value = Settings())
+{
+    if(node.empty())
+        x = default_value;
+    else
+        x.read(node);
+}
+
 int main(int argc, char const *argv[])
 {
     help();
 
     //! [Read configure file]
     Settings s;
+    const string inputConfigFileName = argc > 1? argv[1] : "../default.xml";
+    FileStorage fs(inputConfigFileName, FileStorage::READ);     // Read configurations
+    if (!fs.isOpened())       // Check if configuration file is successfully opened
+    {
+        cout << "Could not open configuration file: " << inputConfigFileName << endl;
+        return -1;
+    }
+    else
+        cout << inputConfigFileName << endl;
+    fs["Settings"] >> s;      //? How does this work?  ?//
+    fs.release();
+
+    if (!s.validInput)
+    {
+        cout << "Invalid input occured. Application stopped." << endl;
+        return -1;
+    }
     //! End of [Read configure file]
 
+    Size imageSize;
+    Mat cameraIntrinsics, distCoeffs;
+    vector< vector<Point2f> > imagePoints;
+    vector<Mat> rvecs, tvecs;
+    vector<float> reprojErr;
+    double totalAvgErr;
+
     //! [Get input]
-    for(int i = 0; ; ++i)
+    for(;;)
     {
         Mat view;
+        
+        view = s.nextImage();
 
-        view = fileRead(i);
+        // namedWindow("chessboard", 0);
+        // resizeWindow("chessboard", 500, 500);
+        // imshow("chessboard", view);
+        // waitKey(100);
+
+        // If view.empty() = true, which indicates that all images in the image list is imported, then calibrate.
         if(view.empty())
         {
+            cout << "starting calibration" << endl;
+            runCalibration(s, imageSize, cameraIntrinsics, distCoeffs, imagePoints, rvecs, tvecs, reprojErr, totalAvgErr);
             break;
         }
-        cout << i << ".jpg read" << endl;
-    }
-    //! End of [Get input]
+        //! End of [Get input]
+        imageSize = view.size();
 
-    //! [Find pattern]
-    bool found;
-    found = findChessboardCorners( view,  )
-    //! End of [Find pattern]
+        //! [Find pattern]
+        bool found;
+        vector<Point2f> pointBuf;
+        found = findChessboardCorners( view, s.boardSize, pointBuf,
+                                       CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_NORMALIZE_IMAGE );
+        //! End of [Find pattern]
+
+        //! [Pattern found]
+        // Improve corner's coordinate accuracy for chess board
+        if (found)
+        {
+            Mat viewGray;
+            cvtColor(view, viewGray, COLOR_BGR2GRAY);
+            cornerSubPix( viewGray, pointBuf, Size(11,11),
+                        Size(-1,-1), TermCriteria( TermCriteria::EPS+TermCriteria::COUNT, 30, 0.1 ));
+            imagePoints.push_back(pointBuf);
+            // Draw the corners
+            drawChessboardCorners(view, s.boardSize, Mat(pointBuf), found);
+            // namedWindow("chessboard", 0);
+            // resizeWindow("chessboard", 1000, 1000);
+            // imshow("chessboard", view);
+            // waitKey(0);
+        }
+        //! End of [Pattern found]
+    }
     
+    cout << cameraIntrinsics << endl;
     return 0;
 }
 
